4cc79a21056df251119cc0799a857ac05b1e32c5
madhan <madhan@DESKTOP-H7GD5R4>
2018-09-07 21:21:21 +0530

    updated code

--- src/main/java/com/madhan/test/CacheConfiguration.java
+++ src/main/java/com/madhan/test/CacheConfiguration.java
@@ -2,6 +2,7 @@ package com.madhan.test;
 
 
 import org.springframework.cache.CacheManager;
+
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
